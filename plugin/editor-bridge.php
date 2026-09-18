ef1d561ac1441e38243a333d28097a141ff57355
wzf2000 <nbjcwzf@163.com>
2026-09-18 18:15:17 +0800

    Load editor bridge on new post admin footer

--- plugin/editor-bridge.php
+++ plugin/editor-bridge.php
@@ -662,5 +662,6 @@ function mbb_enqueue_ui()
 }
 add_action('admin_enqueue_scripts', 'mbb_enqueue_ui');
 add_action('enqueue_block_editor_assets', 'mbb_enqueue_ui');
+add_action('admin_footer-post-new.php', 'mbb_enqueue_ui');
 
 require_once __DIR__ . '/lifecycle.php';
